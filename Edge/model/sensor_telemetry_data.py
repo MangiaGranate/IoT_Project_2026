b9fac70b413b1eac1b69ff4b1e117b2fc6fcf003
Ilir30-lab <ilirrama69@gmail.com>
2026-02-04 18:07:04 +0100

    README modificato

---
+++ Edge/model/sensor_telemetry_data.py
@@ -0,0 +1,11 @@
+import json
+
+class Device:
+    def __init__(self, version, name, id, manufacturer):
+        self.name=name,
+        self.id=id,
+        self.manufacturer=manufacturer,
+        self.version=version
+
+
+
