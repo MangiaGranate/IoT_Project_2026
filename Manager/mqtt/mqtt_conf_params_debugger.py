b57099b695077367fce616b0c293df1980b01029
LF <lucafiaccadori10@gmail.com>
2026-02-08 19:36:26 +0100

    primo test dei dati dal broker (hivemq.com) - aggiunta di mqtt_conf_params_debugger

---
+++ Manager/mqtt/mqtt_conf_params_debugger.py
@@ -0,0 +1,24 @@
+'''
+Mqtt parametri di debugging (LF)
+'''
+
+
+class MqttConfigurationParameters(object):
+    BROKER_ADDRESS = "broker.hivemq.com"
+    BROKER_PORT = 1883
+    MQTT_USERNAME = ""
+    MQTT_PASSWORD = ""
+    MQTT_BASIC_TOPIC = "/iot/user/{0}".format(MQTT_USERNAME)
+    LIFT_TOPIC = "lift"
+    LIFT_TELEMETRY_TOPIC = "telemetry"
+
+
+
+
+'''
+class MqttConfigurationParameters(object):
+    BROKER_ADDRESS = "155.185.4.4"
+    BROKER_PORT = 7883
+    MQTT_USERNAME = "336788@studenti.unimore.it"
+    MQTT_PASSWORD = "qyqgzknbqczucjyp"
+'''
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from Edge.iot_devices import *
-from Manager.mqtt.mqtt_conf_params import MqttConfigurationParameters as MqttConf
+from Manager.mqtt.mqtt_conf_params_debugger import MqttConfigurationParameters as MqttConf
+#from Manager.mqtt.mqtt_conf_params import MqttConfigurationParameters as MqttConf
 from Edge.edge_device import EdgeDevice
 
 
